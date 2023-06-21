f0cbc4b07ccdf6920c7d178c87e3f90216a2405d
Sérgio Araújo <sergiozaraujoz98@gmail.com>
2023-06-21 16:32:35 +0000

    added font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,34 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from "next/head";
+import { PT_Serif } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
+
+const font = PT_Serif({
+  weight: "400",
+  subsets: ["latin"],
+});
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-    <Head>
-      <title>TailoredCV</title>
-      <meta
-          name="description"
-          content="The ultimate app designed to elevate your job application game. Empowers you to create personalized résumés and cover letters for each specific position you apply to."
-      />
-      <meta property="og:image" content="/tailoredBite.png"/>
-      <meta
-          property="og:description"
-          content="The ultimate app designed to elevate your job application game. Empowers you to create personalized résumés and cover letters for each specific position you apply to."
-      />
-      <meta property="og:title" content="TailoredCV"/>
-      <link rel="icon" href="/logo.svg"/>
-    </Head>
-    <Component {...pageProps} />
+
+    <main className={font.className}>
+      <Head>
+        <title>TailoredCV</title>
+        <meta
+            name="description"
+            content="The ultimate app designed to elevate your job application game. Empowers you to create personalized résumés and cover letters for each specific position you apply to."
+        />
+        <meta property="og:image" content="/tailoredBite.png"/>
+        <meta
+            property="og:description"
+            content="The ultimate app designed to elevate your job application game. Empowers you to create personalized résumés and cover letters for each specific position you apply to."
+        />
+        <meta property="og:title" content="TailoredCV"/>
+        <link rel="icon" href="/logo.svg"/>
+      </Head>
+      <Component {...pageProps} />
+      <Analytics />
+    </main>
   </>
 }
