cdbb328eb6ef3b9961c1824ecd14a303d11d838c
Sérgio Araújo <sergiozaraujoz98@gmail.com>
2023-06-22 12:58:52 +0000

    Added terms

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react"
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
@@ -55,11 +55,11 @@ function PrivacyPolicy() {
                 <h2 className={"text-lg pt-4 pb-1 font-bold"}>Log Files</h2>
 
                 <p>TailoredCV follows a standard procedure of using log files. These files log visitors when they visit
-                    websites. All hosting companies do this and a part of hosting services' analytics. The information
+                    websites. All hosting companies do this and a part of hosting services&apos; analytics. The information
                     collected by log files include internet protocol (IP) addresses, browser type, Internet Service
                     Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These
                     are not linked to any information that is personally identifiable. The purpose of the information is
-                    for analyzing trends, administering the site, tracking users' movement on the website, and gathering
+                    for analyzing trends, administering the site, tracking users&apos; movement on the website, and gathering
                     demographic information.</p>
 
 
@@ -70,7 +70,7 @@ function PrivacyPolicy() {
 
                 <p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that
                     are used in their respective advertisements and links that appear on TailoredCV, which are sent
-                    directly to users' browser. They automatically receive your IP address when this occurs. These
+                    directly to users&apos; browser. They automatically receive your IP address when this occurs. These
                     technologies are used to measure the effectiveness of their advertising campaigns and/or to
                     personalize the advertising content that you see on websites that you visit.</p>
 
@@ -79,23 +79,23 @@ function PrivacyPolicy() {
 
                 <h2 className={"text-lg pt-4 pb-1 font-bold"}>Third Party Privacy Policies</h2>
 
-                <p>TailoredCV's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising
+                <p>TailoredCV&apos;s Privacy Policy does not apply to other advertisers or websites. Thus, we are advising
                     you to consult the respective Privacy Policies of these third-party ad servers for more detailed
                     information. It may include their practices and instructions about how to opt-out of certain
                     options. </p>
 
                 <p>You can choose to disable cookies through your individual browser options. To know more detailed
-                    information about cookie management with specific web browsers, it can be found at the browsers'
+                    information about cookie management with specific web browsers, it can be found at the browsers&apos;
                     respective websites.</p>
 
                 <h2 className={"text-lg pt-4 pb-1 font-bold"}>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
 
                 <p>Under the CCPA, among other rights, California consumers have the right to:</p>
-                <p>Request that a business that collects a consumer's personal data disclose the categories and specific
+                <p>Request that a business that collects a consumer&apos;s personal data disclose the categories and specific
                     pieces of personal data that a business has collected about consumers.</p>
                 <p>Request that a business delete any personal data about the consumer that a business has
                     collected.</p>
-                <p>Request that a business that sells a consumer's personal data, not sell the consumer's personal
+                <p>Request that a business that sells a consumer&apos;s personal data, not sell the consumer&apos;s personal
                     data.</p>
                 <p>If you make a request, we have one month to respond to you. If you would like to exercise any of
                     these rights, please contact us.</p>
@@ -120,7 +120,7 @@ function PrivacyPolicy() {
                 <p>If you make a request, we have one month to respond to you. If you would like to exercise any of
                     these rights, please contact us.</p>
 
-                <h2 className={"text-lg pt-4 pb-1 font-bold"}>Children's Information</h2>
+                <h2 className={"text-lg pt-4 pb-1 font-bold"}>Children&apos;s Information</h2>
 
                 <p>Another part of our priority is adding protection for children while using the internet. We encourage
                     parents and guardians to observe, participate in, and/or monitor and guide their online
